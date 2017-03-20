d7289cba8ff4c9f56f5a91f63ec32afb985e26a8
nipa <nipa@codefx.org>
2017-03-20 12:29:25 +0100

    Let monitor.statistics use stats.fancy

--- monitor.statistics/src/main/java/monitor/statistics/Statistician.java
+++ monitor.statistics/src/main/java/monitor/statistics/Statistician.java
@@ -1,20 +1,46 @@
 package monitor.statistics;
 
 import monitor.observer.DiagnosticDataPoint;
+import stats.fancy.FancyStats;
 
+import java.lang.reflect.Layer;
 import java.util.stream.Stream;
 
 public class Statistician {
+
+	private final boolean isFancyAvailable;
+
+	public Statistician() {
+		isFancyAvailable = checkFancyStats();
+	}
+
+	private boolean checkFancyStats() {
+		boolean isFancyAvailable = Layer.boot()
+				.configuration()
+				.findModule("stats.fancy")
+				.isPresent();
+		String message = "Module 'stats.fancy' is"
+				+ (isFancyAvailable ? " " : " not ")
+				+ "available.";
+		System.out.println(message);
+		return isFancyAvailable;
+	}
 
 	public Statistics emptyStatistics() {
 		return Statistics.create();
 	}
 
 	public Statistics compute(Statistics currentStats, Iterable<DiagnosticDataPoint> dataPoints) {
-		Statistics finalStats = currentStats;
-		for (DiagnosticDataPoint dataPoint : dataPoints)
-			finalStats = finalStats.merge(dataPoint);
-		return finalStats;
+		if (isFancyAvailable) {
+			// here, fancystats could actually be used
+			System.out.println(FancyStats.COPYRIGHT);
+			return emptyStatistics();
+		} else {
+			Statistics finalStats = currentStats;
+			for (DiagnosticDataPoint dataPoint : dataPoints)
+				finalStats = finalStats.merge(dataPoint);
+			return finalStats;
+		}
 	}
 
 }
